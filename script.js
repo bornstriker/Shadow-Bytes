333685beea8732312a2fa0da48cc49764c19257f
bornstriker <158768455+bornstriker@users.noreply.github.com>
2024-03-08 16:23:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function validateLogin() {
         // fireworksEffect();
         setTimeout(function () {
             alert("PARABÉNS! Sua missão foi concluída com sucesso!\nFale com os administradores para reivindicar sua recompensa.");
+            window.open("indexs.html");
         }, 5000);  // Atraso de 5 segundos para dar tempo para os efeitos
         return false;  // Retorne false para evitar a submissão do formulário padrão
     } else {
